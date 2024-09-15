c2041baf388bb1482c1565305dbb7e8c9be7c9cd
Moein Moeinnia <Moeinnia@protonmail.com>
2024-09-15 21:21:35 +0330

    chore: deploy script added

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
+docker kill hamback
+echo y | docker system prune
+docker pull hamyarchat/back:latest
 docker run -d --name hamback -v hamback-logs:/app/dist/logs -p 12000:12000 --restart unless-stopped hamyarchat/back:latest
+docker ps
