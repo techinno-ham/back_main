1400be0bd3c2448dc349578a2c229bf2cbf8d6eb
Moein Moeinnia <Moeinnia@protonmail.com>
2024-12-12 19:31:43 +0330

    chore: minimal structure of live services and model logic added

--- src/v1/live/live.controller.ts
+++ src/v1/live/live.controller.ts
@@ -1,4 +1,98 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    HttpException,
+    Post,
+    Headers,
+    HttpStatus,
+    Logger,
+  } from '@nestjs/common';
+  import {
+    Body,
+    Delete,
+    Ip,
+    Param,
+    Query,
+    Req,
+    Res,
+    UploadedFiles,
+    UseGuards,
+    UseInterceptors,
+  } from '@nestjs/common/decorators';
+import { ChatSessionId } from '../decorators/chatSession.decorator';
+import { v4 as uuidv4 } from 'uuid';
+import { LiveService } from './live.service';
 
 @Controller('live')
-export class LiveController {}
+export class LiveController {
+    private readonly logger = new Logger(LiveController.name);
+
+    constructor(
+        private readonly liveService: LiveService,
+    ){}
+
+    @Post('/request-chat')
+    async requestLiveConversation(
+      @Param('botId') botId: string,
+      @ChatSessionId() sessionId: string, 
+      @Ip() userIP: string,
+      @Res() res: Response,
+    ) {
+      this.logger.log(`Creating conversation for bot ID: ${botId}, user IP: ${userIP}`);
+
+      try {
+        console.log(sessionId,"sessionId")
+        if (!sessionId) {
+          sessionId = uuidv4();  // Generate a new session ID
+          const oneDayMaxAge = 7 * 24 * 60 * 60 * 1000;  // 1 day in milliseconds
+          res.cookie('widget_session_id', sessionId, {
+            maxAge: oneDayMaxAge,
+            httpOnly: true,
+            secure: true, 
+          });
+          this.logger.log(`New session ID created and set in cookie: ${sessionId}`);
+        } else {
+          this.logger.log(`Existing session ID found: ${sessionId}`);
+        }
+    
+        // Create the conversation
+        const createConversation = await this.mybotsServices.createConversation({
+          botId,
+          widgetVersion,
+          sessionId: sessionId,
+          userIP,
+        });
+    
+        this.logger.log(`Conversation created successfully for bot ID: ${botId}, conversation ID: ${createConversation.conversationId}`);
+        return res.json({ conversationId: createConversation.conversationId,sessionId:sessionId });
+      } catch (error) {
+        this.logger.error(`Error creating conversation for bot ID: ${botId}, user IP: ${userIP}`, error.stack);
+        throw new HttpException('Failed to create conversation. Please try again later.', HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
+
+    //SORT BY TIME
+    @Get('/history')
+    async fetchLiveConversationHistory(){}
+
+    //CHECK IF CONV EXISTS FOR SECRITY
+    @Post('/message/user')
+    async receiveUserMessage(){}
+
+    //CHECK IF CONV EXISTS FOR SECRITY
+    @Post('/message/operator')
+    async receiveOperatorMessage(){}
+
+    //SORT BY TIME
+    @Get('/messages/user')
+    async fetchUserMessage(){}
+
+    //SORT BY TIME
+    @Get('/messages/operator')
+    async fetchOperatorMessage(){}
+
+    //FETCH CONVERSATION IDS THAT HAVE LIVE CHAT - SHOWING ACTIVE LIVE IN PANEL
+    @Get('/conversation-ids')
+    async fetchBotLiveConversations(){}
+}
+
--- src/v1/live/live.service.ts
+++ src/v1/live/live.service.ts
@@ -1,4 +1,108 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 
 @Injectable()
-export class LiveService {}
+export class LiveService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async requestLiveConversationService() {
+    try {
+      let transactionResult = await this.prismaService.$transaction([
+        this.prismaService.conversations.update({
+          where: {
+            conversation_id: 'abc',
+          },
+          //SHOULD UPDATE isLiveRequested
+          data: undefined,
+        }),
+        this.prismaService.live_chat_conversations.create({
+          data: {
+            conversation_id: 'a',
+            bot_id: 'b',
+            session_id: 'c',
+          },
+        }),
+      ]);
+      const [] = await Promise.all([]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async receiveUserMessageService() {
+    try {
+        let result = await this.prismaService.live_chat_messages.create({
+            data: {
+                conversation_id : "",
+                sender: "user",
+                message: "",
+
+            }
+        })
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  async receiveOperatorMessageService() {
+    try {
+        let result = await this.prismaService.live_chat_messages.create({
+            data: {
+                conversation_id : "",
+                sender: "operator",
+                message: "",
+
+            }
+        })
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  async fetchLiveConversationHistoryService() {
+    try {
+      let result = await this.prismaService.live_chat_conversations.findMany({
+        where: {
+          conversation_id: 'a',
+          bot_id: 'b',
+          session_id: 'c',
+        },
+        include: {
+          live_chat_messages: {
+            orderBy: {
+              sent_at: 'asc',
+            },
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async fetchUserMessageService() {}
+
+  async fetchOperatorMessageService() {}
+
+  async fetchBotLiveConversationsService() {
+    try {
+      let result = await this.prismaService.bots.findMany({
+        where: {
+          bot_id: 'a',
+        },
+        include: {
+          live_chat_conversations: {
+            where: {
+              conversation_id: 'b',
+            },
+            select: {
+              conversation_id: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
